572037a25bd47f60506b78b7d23d630f0dbca4d4
gilchavarria <gilbert@fuzzyredpanda.com>
2024-08-10 15:21:30 -0600

    No changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,6 @@ function App() {
       </div>
       <h1>FFMPEG Test</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
